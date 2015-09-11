a7a8405c094de501aa690751bf04bf772eb3efba
unknown <LATAM1@697-334-Sauron.sf.k12.internal>
2015-09-11 11:11:15 -0700

    Dice added

--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -1,10 +1,26 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Dice extends PApplet {
+
 Die woo;
-void setup()
+public void setup()
 {
 	noLoop();
 	size(490,500);
 }
-void draw()
+public void draw()
 {
 	background(0);
 	for(int y=10; y <= 400; y+=60)
@@ -17,7 +33,7 @@ void draw()
 	}
 }
 
-void mousePressed()
+public void mousePressed()
 {
 	redraw();
 }
@@ -30,11 +46,11 @@ class Die
 		myY = y;
 		r= (int)(Math.random()*6)+1;
 	}
-	void roll()
+	public void roll()
 	{
 		r= (int)(Math.random()*6)+1;
 	}
-	void show()
+	public void show()
 	{
 		fill (255);
 		rect(myX, myY, 50,50);
@@ -79,4 +95,13 @@ class Die
 			ellipse(myX + 40, myY+25,5,5);
 		}
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Dice" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
